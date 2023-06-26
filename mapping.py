5d19a3c3da2de81d6a88ee032c2091c9a5ca815c
Esrael Dawit <79444726+EsraelDawit-a@users.noreply.github.com>
2023-06-26 09:53:16 +0300

    Update mapping.py

---
+++ mapping.py
@@ -0,0 +1,138 @@
+import sys
+import json
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl, QObject, pyqtSignal, pyqtSlot
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebChannel import QWebChannel
+
+mapbox_token = 'your_mapbox_token'
+
+html = """
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body { margin: 0; padding: 0; }
+        #map { position: absolute; top: 0; bottom: 0; width: 100%; }
+    </style>
+</head>
+<body>
+<div id='map'></div>
+<script src='https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css' rel='stylesheet' />
+<script src="qrc:///qtwebchannel/qwebchannel.js"></script>
+<script>
+    mapboxgl.accessToken = '%s';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-96, 37.8],
+        zoom: 3
+    });
+
+    var startMarker = new mapboxgl.Marker({color: 'green'})
+    var endMarker = new mapboxgl.Marker({color: 'red'})
+    var carMarker = new mapboxgl.Marker({color: 'blue'})
+
+    map.on('click', function(e) {
+        window.pyObj.mapClicked(e.lngLat.lng, e.lngLat.lat)
+    });
+
+    new QWebChannel(qt.webChannelTransport, function (channel) {
+        window.pyObj = channel.objects.pyObj;
+    });
+
+    function updateMarker(longitude, latitude) {
+        carMarker.setLngLat([longitude, latitude]).addTo(map);
+    }
+
+    function setStart(longitude, latitude) {
+        startMarker.setLngLat([longitude, latitude]).addTo(map);
+    }
+
+    function setEnd(longitude, latitude) {
+        endMarker.setLngLat([longitude, latitude]).addTo(map);
+    }
+
+    function setRoute(coordinates) {
+        if (map.getSource('route')) {
+            map.removeLayer('route');
+            map.removeSource('route');
+        }
+
+        map.addSource('route', {
+            'type': 'geojson',
+            'data': {
+                'type': 'Feature',
+                'properties': {},
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': coordinates
+                }
+            }
+        });
+
+        map.addLayer({
+            'id': 'route',
+            'type': 'line',
+            'source': 'route',
+            'layout': {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            'paint': {
+                'line-color': 'blue',
+                'line-width': 6
+            }
+        });
+    }
+</script>
+</body>
+</html>
+""" % mapbox_token
+
+class MapboxApp(QObject):
+    def __init__(self):
+        super().__init__()
+
+        self.view = QWebEngineView()
+        self.view.setHtml(html)
+
+        self.channel = QWebChannel()
+        self.channel.registerObject("pyObj", self)
+        self.view.page().setWebChannel(self.channel)
+
+        self.start = None
+        self.end = None
+        self.route = None
+
+    @pyqtSlot(float, float)
+    def mapClicked(self, longitude, latitude):
+        if not self.start:
+            self.start = [longitude, latitude]
+            self.view.page().runJavaScript("setStart(%f, %f)" % (longitude, latitude))
+        elif not self.end:
+            self.end = [longitude, latitude]
+            self.view.page().runJavaScript("setEnd(%f, %f)" % (longitude, latitude))
+            self.calculateAndSetRoute()
+
+    def calculateAndSetRoute(self):
+        # Replace with actual call to Mapbox Directions API
+        self.route = [self.start, self.end]
+        self.view.page().runJavaScript("setRoute(%s)" % json.dumps(self.route))
+        self.moveCarAlongRoute()
+
+    def moveCarAlongRoute(self):
+        # Simulate car movement
+        for coord in self.route:
+            self.view.page().runJavaScript("updateMarker(%f, %f)" % (coord[0], coord[1]))
+            QApplication.processEvents()
+            QThread.msleep(500)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    mapboxApp = MapboxApp()
+    mapboxApp.view.show()
+
+    sys.exit(app.exec_())
